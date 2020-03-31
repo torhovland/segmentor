07121bbcc27f6da3b9706a706dd2fef8cfd4690a
Tor Hovland <tor.hovland@bekk.no>
2020-03-31 20:32:00 +0200

    Passing flags to the Elm app.

--- src/Main.elm
+++ src/Main.elm
@@ -1,12 +1,14 @@
 module Main exposing (main)
 
 import Browser
+import Browser.Navigation
 import Html exposing (Html, a, button, div, h1, img, text)
 import Html.Attributes exposing (href, src)
 import Html.Events exposing (onClick)
+import Url
 
 
-main : Program () Int Msg
+main : Program Flags Model Msg
 main =
     Browser.application
         { init = init
@@ -18,33 +20,48 @@ main =
         }
 
 
+type alias OAuthParameters =
+    { clientId : String
+    , clientSecret : String
+    }
+
+
+type alias Model =
+    { oauth : OAuthParameters, number : Int }
+
+
+type alias Flags =
+    { x : Float, y : Float }
+
+
 type Msg
     = Increment
     | Decrement
 
 
+init : Flags -> Url.Url -> Browser.Navigation.Key -> ( Model, Cmd Msg )
 init flags url key =
-    ( 0, Cmd.none )
+    ( { oauth = { clientId = "foo", clientSecret = "bar" }, number = 0 }, Cmd.none )
 
 
-update : Msg -> Int -> ( Int, Cmd Msg )
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Increment ->
-            ( model + 1, Cmd.none )
+            ( { model | number = model.number + 1 }, Cmd.none )
 
         Decrement ->
-            ( model - 1, Cmd.none )
+            ( { model | number = model.number - 1 }, Cmd.none )
 
 
-view : Int -> Browser.Document Msg
+view : Model -> Browser.Document Msg
 view model =
     { title = "foo"
     , body =
         [ h1 [] [ text "KOM.one" ]
         , div [] [ a [ href "https://www.strava.com/oauth/authorize?client_id=" ] [ img [ src "images/btn_strava_connectwith_orange.svg" ] [] ] ]
         , button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (String.fromInt model) ]
+        , div [] [ text (String.fromInt model.number) ]
         , button [ onClick Increment ] [ text "+" ]
         , div [] [ img [ src "images/api_logo_pwrdBy_strava_horiz_light.svg" ] [] ]
         ]
