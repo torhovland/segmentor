4d405ccf0c50382c24a8db5e8ac54386a871cdfe
Tor Hovland <tor@hovland.co>
2020-04-19 10:34:14 +0200

    Got saving to local storage working.

--- src/Main.elm
+++ src/Main.elm
@@ -2,12 +2,22 @@ module Main exposing (main)
 
 import Browser
 import Browser.Navigation as Nav
-import Html exposing (a, button, div, h1, img, text)
-import Html.Attributes exposing (href, src)
-import Html.Events exposing (onClick)
+import Cmd.Extra exposing (addCmd, addCmds, withCmd, withCmds, withNoCmd)
+import Dict exposing (Dict)
+import Html exposing (Html, a, b, button, div, h1, img, input, p, table, td, text, tr)
+import Html.Attributes exposing (href, src, style, type_, value)
+import Html.Events exposing (onClick, onInput)
 import Http
 import Json.Decode as Decode
 import Json.Decode.Extra as Decode
+import Json.Encode as Encode exposing (Value)
+import Json.Encode.Extra as Encode
+import PortFunnel.LocalStorage as LocalStorage
+    exposing
+        ( Message
+        , Response(..)
+        )
+import PortFunnels exposing (FunnelDict, Handler(..))
 import Time
 import Url
 
@@ -16,7 +26,7 @@ main : Program Flags Model Msg
 main =
     Browser.application
         { init = init
-        , subscriptions = subscriptions
+        , subscriptions = PortFunnels.subscriptions Process
         , update = update
         , view = view
         , onUrlChange = UrlChanged
@@ -24,8 +34,12 @@ main =
         }
 
 
+type alias ActivityId =
+    Int
+
+
 type alias Activity =
-    { id : Int
+    { id : ActivityId
     , time : Time.Posix
     , name : String
     , activityType : String
@@ -34,6 +48,10 @@ type alias Activity =
     , private : Bool
     , gearId : Maybe String
     }
+
+
+type alias ActivityDict =
+    Dict ActivityId Activity
 
 
 type alias AccessToken =
@@ -59,8 +77,16 @@ type alias StravaAuth =
 
 type alias Model =
     { key : Nav.Key
+    , storageKey : LocalStorage.Key
+    , value : String
+    , label : String
+    , returnLabel : String
+    , keysString : String
+    , useSimulator : Bool
     , url : Url.Url
-    , error : String
+    , wasLoaded : Bool
+    , funnelState : PortFunnels.State
+    , error : Maybe String
     , status : Status
     , stravaAuth : Maybe (Result Decode.Error StravaAuth)
     , activities : List Activity
@@ -80,6 +106,15 @@ type Msg
     = LinkClicked Browser.UrlRequest
     | UrlChanged Url.Url
     | GotActivities (Result Http.Error (List Activity))
+    | SetKey String
+    | SetValue String
+    | SetLabel String
+    | GetItem
+    | SetItem
+    | ListKeys
+    | RemoveItem
+    | Clear
+    | Process Value
     | Increment
     | Decrement
 
@@ -114,13 +149,32 @@ init flags url key =
 
         currentPageNumber =
             0
+
+        model =
+            { key = key
+            , storageKey = "key"
+            , value = ""
+            , label = ""
+            , returnLabel = ""
+            , keysString = ""
+            , useSimulator = True
+            , url = url
+            , wasLoaded = False
+            , funnelState = PortFunnels.initialState "example"
+            , error = Nothing
+            , status = Idle
+            , stravaAuth = stravaAuth
+            , activities = []
+            , activityPageNumber = currentPageNumber
+            , number = 0
+            }
     in
     case stravaAuth of
         Just (Ok auth) ->
-            ( Model key url "" DownloadingActivities stravaAuth [] currentPageNumber 0, getNextActivityPage auth.accessToken currentPageNumber )
+            ( { model | status = DownloadingActivities }, getNextActivityPage auth.accessToken currentPageNumber )
 
         _ ->
-            ( Model key url "" Idle stravaAuth [] currentPageNumber 0, Cmd.none )
+            ( model, Cmd.none )
 
 
 getNextActivityPage : AccessToken -> PageNumber -> Cmd Msg
@@ -155,6 +209,132 @@ errorToString err =
             "Malformed url: " ++ url
 
 
+getCmdPort : String -> Model -> (Value -> Cmd Msg)
+getCmdPort moduleName model =
+    PortFunnels.getCmdPort Process moduleName model.useSimulator
+
+
+send : Message -> Model -> Cmd Msg
+send message model =
+    LocalStorage.send (getCmdPort LocalStorage.moduleName model)
+        message
+        model.funnelState.storage
+
+
+decodeString : Value -> String
+decodeString value =
+    case Decode.decodeValue Decode.string value of
+        Ok res ->
+            res
+
+        Err err ->
+            Decode.errorToString err
+
+
+storageKey : Activity -> String
+storageKey activity =
+    "a" ++ String.fromInt activity.id
+
+
+saveActivity : Model -> Activity -> Cmd Msg
+saveActivity model activity =
+    send
+        (LocalStorage.put (storageKey activity)
+            (Just <|
+                Encode.object
+                    [ ( "id", Encode.int activity.id )
+                    , ( "name", Encode.string activity.name )
+                    , ( "time", Encode.int (Time.posixToMillis activity.time) )
+                    ]
+            )
+        )
+        model
+
+
+saveActivities : Model -> List Activity -> Cmd Msg
+saveActivities model activities =
+    List.map (saveActivity model) activities |> Cmd.batch
+
+
+doIsLoaded : Model -> Model
+doIsLoaded model =
+    if not model.wasLoaded && LocalStorage.isLoaded model.funnelState.storage then
+        { model
+            | useSimulator = False
+            , wasLoaded = True
+        }
+
+    else
+        model
+
+
+storageHandler : LocalStorage.Response -> PortFunnels.State -> Model -> ( Model, Cmd Msg )
+storageHandler response state mdl =
+    let
+        model =
+            doIsLoaded
+                { mdl | funnelState = state }
+    in
+    case response of
+        LocalStorage.GetResponse { label, key, value } ->
+            let
+                string =
+                    case value of
+                        Nothing ->
+                            "<null>"
+
+                        Just v ->
+                            decodeString v
+            in
+            ( { model
+                | storageKey = key
+                , value = string
+                , returnLabel =
+                    case label of
+                        Nothing ->
+                            "Nothing"
+
+                        Just lab ->
+                            "Just \"" ++ lab ++ "\""
+              }
+            , Cmd.none
+            )
+
+        LocalStorage.ListKeysResponse { label, keys } ->
+            let
+                keysString =
+                    stringListToString keys
+            in
+            ( { model
+                | keysString = keysString
+                , returnLabel =
+                    case label of
+                        Nothing ->
+                            "Nothing"
+
+                        Just lab ->
+                            "Just \"" ++ lab ++ "\""
+              }
+            , Cmd.none
+            )
+
+        _ ->
+            ( model, Cmd.none )
+
+
+stringListToString : List String -> String
+stringListToString list =
+    let
+        quoted =
+            List.map (\s -> "\"" ++ s ++ "\"") list
+
+        commas =
+            List.intersperse ", " quoted
+                |> String.concat
+    in
+    "[" ++ commas ++ "]"
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
@@ -175,7 +355,7 @@ update msg model =
                     case model.stravaAuth of
                         Just (Ok auth) ->
                             if List.length activities > 0 then
-                                ( { model | activities = model.activities ++ activities, activityPageNumber = model.activityPageNumber + 1 }, getNextActivityPage auth.accessToken model.activityPageNumber )
+                                ( { model | activities = model.activities ++ activities, activityPageNumber = model.activityPageNumber + 1 }, Cmd.batch [ getNextActivityPage auth.accessToken model.activityPageNumber, saveActivities model activities ] )
 
                             else
                                 ( { model | activityPageNumber = 0, status = Idle }, Cmd.none )
@@ -184,13 +364,84 @@ update msg model =
                             ( { model | activityPageNumber = 0, status = Idle }, Cmd.none )
 
                 Err err ->
-                    ( { model | activityPageNumber = 0, status = Idle, error = errorToString err }, Cmd.none )
+                    ( { model | activityPageNumber = 0, status = Idle, error = Just (errorToString err) }, Cmd.none )
+
+        SetKey key ->
+            { model | storageKey = key } |> withNoCmd
+
+        SetValue value ->
+            { model | value = value } |> withNoCmd
+
+        SetLabel label ->
+            { model | label = label } |> withNoCmd
+
+        GetItem ->
+            let
+                message =
+                    if model.label == "" then
+                        LocalStorage.get model.storageKey
+
+                    else
+                        LocalStorage.getLabeled model.label model.storageKey
+            in
+            { model | returnLabel = "" }
+                |> withCmd (send message model)
+
+        SetItem ->
+            { model | returnLabel = "" }
+                |> withCmd
+                    (send
+                        (LocalStorage.put model.storageKey
+                            (Just <| Encode.string model.value)
+                        )
+                        model
+                    )
+
+        ListKeys ->
+            let
+                message =
+                    if model.label == "" then
+                        LocalStorage.listKeys model.storageKey
+
+                    else
+                        LocalStorage.listKeysLabeled model.label model.storageKey
+            in
+            { model | returnLabel = "" }
+                |> withCmd (send message model)
+
+        RemoveItem ->
+            { model | returnLabel = "" }
+                |> withCmd
+                    (send (LocalStorage.put model.storageKey Nothing) model)
+
+        Clear ->
+            { model | returnLabel = "" }
+                |> withCmd
+                    (send (LocalStorage.clear model.storageKey) model)
+
+        Process value ->
+            case
+                PortFunnels.processValue funnelDict
+                    value
+                    model.funnelState
+                    model
+            of
+                Err error ->
+                    { model | error = Just error } |> withNoCmd
+
+                Ok res ->
+                    res
 
         Increment ->
             ( { model | number = model.number + 1 }, Cmd.none )
 
         Decrement ->
             ( { model | number = model.number - 1 }, Cmd.none )
+
+
+funnelDict : FunnelDict Model Msg
+funnelDict =
+    PortFunnels.makeFunnelDict [ LocalStorageHandler storageHandler ] getCmdPort
 
 
 rootUrl : Url.Url -> String
@@ -252,9 +503,24 @@ statusBanner model =
             div [] [ text ("Downloading activity page " ++ String.fromInt (model.activityPageNumber + 1)) ]
 
 
+errorBanner : Maybe String -> Html.Html msg
+errorBanner error =
+    case error of
+        Just err ->
+            div [] [ text err ]
+
+        Nothing ->
+            div [] []
+
+
 activityList : List Activity -> List (Html.Html msg)
 activityList list =
     List.map (\a -> div [] [ text a.name ]) list
+
+
+b : String -> Html msg
+b string =
+    Html.b [] [ text string ]
 
 
 view : Model -> Browser.Document Msg
@@ -263,8 +529,83 @@ view model =
     , body =
         [ h1 [] [ text "KOM.one" ]
         , authBanner model
-        , div [] [ text model.error ]
+        , errorBanner model.error
         , statusBanner model
+        , p []
+            [ table []
+                [ tr []
+                    [ td [] [ b "Key: " ]
+                    , td []
+                        [ input
+                            [ value model.storageKey
+                            , onInput SetKey
+                            ]
+                            []
+                        ]
+                    ]
+                , tr []
+                    [ td [] [ b "Value: " ]
+                    , td []
+                        [ input
+                            [ value model.value
+                            , onInput SetValue
+                            ]
+                            []
+                        ]
+                    ]
+                , tr []
+                    [ td [] [ b "Label: " ]
+                    , td []
+                        [ input
+                            [ value model.label
+                            , onInput SetLabel
+                            ]
+                            []
+                        ]
+                    ]
+                , tr []
+                    [ td [] [ b "Return Label: " ]
+                    , td []
+                        [ input
+                            [ value model.returnLabel
+                            , onInput SetLabel
+                            ]
+                            []
+                        ]
+                    ]
+                , tr []
+                    [ td [] [ b "Keys:" ]
+                    , td []
+                        [ text model.keysString ]
+                    ]
+                , tr []
+                    [ td [] [ b "Simulated:" ]
+                    , td []
+                        [ text <|
+                            if model.useSimulator then
+                                "Yes"
+
+                            else
+                                "No"
+                        ]
+                    ]
+                , tr []
+                    [ td [] []
+                    , td []
+                        [ button [ onClick GetItem ]
+                            [ text "Gets" ]
+                        , button [ onClick SetItem ]
+                            [ text "Set" ]
+                        , button [ onClick ListKeys ]
+                            [ text "List" ]
+                        , button [ onClick RemoveItem ]
+                            [ text "Remove" ]
+                        , button [ onClick Clear ]
+                            [ text "Clear" ]
+                        ]
+                    ]
+                ]
+            ]
         , div [] (activityList model.activities)
         , button [ onClick Decrement ] [ text "-" ]
         , div [] [ text (String.fromInt model.number) ]
@@ -272,7 +613,3 @@ view model =
         , div [] [ img [ src "images/api_logo_pwrdBy_strava_horiz_light.svg" ] [] ]
         ]
     }
-
-
-subscriptions model =
-    Sub.none
