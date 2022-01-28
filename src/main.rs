c6d6c8b2b635a92b9be803383097113513b4e4bc
Tor Hovland <55164+torhovland@users.noreply.github.com>
2022-01-28 18:15:03 +0100

    Read from Strava.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+extern crate strava;
+
+use strava::athletes::Athlete;
+use strava::api::AccessToken;
+
+#[tokio::main]
+async fn main() {
+    let token = AccessToken::new("<my token>".to_string());
+
+    // Get the athlete associated with the given token
+    let athlete = Athlete::get_current(&token).await.unwrap();
+
+    // All of the strava types implement Debug and can be printed like so:
+    println!("{:?}", athlete);
 }
