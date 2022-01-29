92445cb3b9045c9bf6fc871d621fd4ac2afb5a3b
Tor Hovland <55164+torhovland@users.noreply.github.com>
2022-01-29 18:47:35 +0100

    Read access token from local file.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,33 @@
 extern crate strava;
 
-use strava::athletes::Athlete;
+use anyhow::{Context, Result};
+use std::path::Path;
+use std::{env, fs};
+
 use strava::api::AccessToken;
+use strava::athletes::Athlete;
 
 #[tokio::main]
-async fn main() {
-    let token = AccessToken::new("<my token>".to_string());
+async fn main() -> Result<()> {
+    let token = read_token().with_context(|| "Failed to read Strava access token.")?;
 
-    // Get the athlete associated with the given token
-    let athlete = Athlete::get_current(&token).await.unwrap();
+    let athlete = Athlete::get_current(&token)
+        .await
+        .with_context(|| "Failed to get athlete data from Strava.")?;
 
-    // All of the strava types implement Debug and can be printed like so:
     println!("{:?}", athlete);
+
+    Ok(())
+}
+
+fn read_token() -> Result<AccessToken> {
+    Ok(AccessToken::new(
+        fs::read_to_string(
+            Path::new(&env::var("HOME")?)
+                .join(".segmentor")
+                .join("access-token"),
+        )?
+        .trim()
+        .to_string(),
+    ))
 }
