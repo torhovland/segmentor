eaabc684b31b54a5599f2e90ba29624d29b1f6c0
Tor Hovland <55164+torhovland@users.noreply.github.com>
2022-02-06 19:10:31 +0100

    Deploy a Rocket app to Google Cloud Run using GitHub Actions.

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,73 @@
 extern crate strava;
 
-use anyhow::{Context, Result};
-use std::path::Path;
-use std::{env, fs};
+// use anyhow::{Context, Result};
+// use oauth2::basic::BasicClient;
+// use oauth2::reqwest::async_http_client;
+// use oauth2::{AuthUrl, ClientId, ClientSecret, Scope, TokenUrl};
+// use std::path::Path;
+// use std::{env, fs};
+// use strava::activities::Activity;
+// use strava::api::AccessToken;
+// use strava::athletes::Athlete;
 
-use strava::api::AccessToken;
-use strava::athletes::Athlete;
+#[macro_use]
+extern crate rocket;
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    let token = read_token().with_context(|| "Failed to read Strava access token.")?;
-
-    let athlete = Athlete::get_current(&token)
-        .await
-        .with_context(|| "Failed to get athlete data from Strava.")?;
-
-    println!("{:?}", athlete);
-
-    Ok(())
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
 }
 
-fn read_token() -> Result<AccessToken> {
-    Ok(AccessToken::new(
-        fs::read_to_string(
-            Path::new(&env::var("HOME")?)
-                .join(".segmentor")
-                .join("access-token"),
-        )?
-        .trim()
-        .to_string(),
-    ))
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
+
+// #[tokio::main]
+// async fn main() -> Result<()> {
+//     let client = BasicClient::new(
+//         ClientId::new("38457".to_string()),
+//         Some(ClientSecret::new(
+//             "1fb09e3b762ba505e6ff0922c04ab2e0ff8bafb3".to_string(),
+//         )),
+//         AuthUrl::new("http://authorize".to_string())?,
+//         Some(TokenUrl::new(
+//             "https://www.strava.com/oauth/token".to_string(),
+//         )?),
+//     );
+
+//     let _token_result = client
+//         .exchange_client_credentials()
+//         .add_scope(Scope::new("read".to_string()))
+//         .request_async(async_http_client)
+//         .await
+//         .with_context(|| "Failed to get access token from Strava.")?;
+
+//     let token = read_token().with_context(|| "Failed to read Strava access token.")?;
+
+//     let athlete = Athlete::get_current(&token)
+//         .await
+//         .with_context(|| "Failed to get athlete from Strava.")?;
+
+//     println!("{:?}", athlete);
+
+//     let activities = Activity::athlete_activities(&token)
+//         .await
+//         .with_context(|| "Failed to get activities from Strava.")?;
+
+//     println!("{:?}", activities);
+
+//     Ok(())
+// }
+
+// fn read_token() -> Result<AccessToken> {
+//     Ok(AccessToken::new(
+//         fs::read_to_string(
+//             Path::new(&env::var("HOME")?)
+//                 .join(".segmentor")
+//                 .join("access-token"),
+//         )?
+//         .trim()
+//         .to_string(),
+//     ))
+// }
