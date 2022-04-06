46c9a35c66f15dec842e5bfdb600c81d9ca1f200
Tor Hovland <55164+torhovland@users.noreply.github.com>
2022-04-06 13:09:35 +0200

    Use crate2nix for incremental builds.

--- default.nix
+++ default.nix
@@ -1,29 +1,13 @@
 let
-  overlay = self: super: {
-    segmentor = self.rustPlatform.buildRustPackage {
-      pname = "segmentor";
-      version = "0.0.1";
-      src = ./.;
-      nativeBuildInputs = [ self.pkgs.pkg-config ];
-      buildInputs = [ self.pkgs.openssl.dev ];
-      cargoLock = {
-        lockFile = ./Cargo.lock;
-        outputHashes = {
-          "strava-0.2.0" = "sha256-K++qwPNd5m9hj4/Wc0OZM2G7b4XlJEJQvXiA8nKIw88=";
-        };
-      };
-    };
-  };
-  hostPkgs = import <nixpkgs> { overlays = [ overlay ]; };
-  dockerPkgs = import <nixpkgs> { overlays = [ overlay ]; system = "x86_64-linux"; };
+  hostPkgs = import <nixpkgs> { };
+  dockerPkgs = import <nixpkgs> { system = "x86_64-linux"; };
+  cargo_nix = import ./Cargo.nix { inherit (dockerPkgs) pkgs; };
 in
 {
-  inherit (hostPkgs) segmentor;
-
   docker = hostPkgs.dockerTools.streamLayeredImage {
     name = "segmentor";
-    contents = [ dockerPkgs.segmentor ];
-    config.Cmd = [ "segmentor" ];
+    contents = [ cargo_nix.rootCrate.build ];
+    config.Cmd = [ "/bin/segmentor" ];
     config.Env = [ "ENVIRONMENT=production" ];
   };
 }
