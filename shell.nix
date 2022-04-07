a8c0bb083508bc5a9165ba6ccaeb6a481fa0550a
Tor Hovland <55164+torhovland@users.noreply.github.com>
2022-04-07 08:16:06 +0200

    yarn

--- shell.nix
+++ shell.nix
@@ -7,6 +7,7 @@ pkgs.mkShell {
     rustfmt 
     clippy 
     openssl.dev # Needed to build the app.
+    yarn
     vscode
     gtk3 # Needed for VS Code code actions, along with the shellHook below. See https://nixos.wiki/wiki/Development_environment_with_nix-shell.
   ];
