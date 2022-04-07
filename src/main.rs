55ab42b459f89e94fc2b7d1e920ef91ff994d5b2
Tor Hovland <tor+github@hovland.co>
2022-04-07 18:36:51 +0200

    Adjust log levels.

--- src/main.rs
+++ src/main.rs
@@ -41,12 +41,8 @@ struct StravaAuth {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // initialize tracing
     let subscriber = FmtSubscriber::builder()
-        // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
-        // will be written to stdout.
-        .with_max_level(Level::TRACE)
-        // completes the builder.
+        .with_env_filter("info,segmentor=trace")
         .finish();
 
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
