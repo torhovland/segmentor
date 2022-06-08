8fce26d82a16a5773ccac63a984dc0af462ba3da
Tor Hovland <55164+torhovland@users.noreply.github.com>
2022-06-08 15:49:19 +0200

    Get activities in frontend using react-query.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,22 @@
+import { QueryClient, QueryClientProvider } from 'react-query';
+import Activities from './Activities';
 import './App.css';
 import Login from './Login';
 import Sync from './Sync';
 
+const queryClient = new QueryClient()
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Login></Login>
-        <Sync></Sync>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <header className="App-header">
+          <Login></Login>
+          <Sync></Sync>
+          <Activities></Activities>
+        </header>
+      </div>
+    </QueryClientProvider>
   );
 }
 
